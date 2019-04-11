6a95b19ab18894fa3a8ebbcbeb311e78a533f5b7
Alex Diwa <atdiwa@gmail.com>
2019-04-11 11:58:45 +1000

    solution for cw_spiral.rb

---
+++ ruby/3_kyu/cw_spiral.rb
@@ -0,0 +1,20 @@
+def spiralize(size)
+  return [] if size == 0
+  return [[1]] if size == 1
+
+  seed = [[1, 1], [0, 0]] if size % 2 != 0
+  seed = [[0]] if size % 2 == 0
+
+  while seed.flatten.length < ((size - 1) * (size - 1))
+    seed = seed.reverse.transpose
+    seed.push(Array.new(seed[0].length, 0))
+    seed.unshift(Array.new(seed[0].length, 1))
+  end
+  
+  2.times do
+    seed = seed.reverse.transpose
+    seed.unshift(Array.new(seed[0].length, 1))
+  end
+
+  seed
+end
